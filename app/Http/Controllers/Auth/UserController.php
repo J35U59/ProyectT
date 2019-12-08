47a28ccc5a07e4e08d8c9fe6c53a2859a55bc98d
J35U59 <jasonx106@gmail.com>
2019-12-08 17:51:04 -0400

    first commit

---
+++ app/Http/Controllers/Auth/UserController.php
@@ -0,0 +1,46 @@
+<?php
+namespace App\Http\Controllers;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+class UserController extends Controller
+{
+    public function index()
+    {
+        //$users = DB::table('users')->get();
+        $users = User::all();
+        $title = 'Listado de usuarios';
+//        return view('users.index')
+//            ->with('users', User::all())
+//            ->with('title', 'Listado de usuarios');
+        return view('users.index', compact('title', 'users'));
+    }
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+    public function create()
+    {
+        return view('users.create');
+    }
+    public function store()
+    {
+        $data = request()->validate([
+            'name' => 'required',
+            'email' => ['required', 'email', 'unique:users,email'],
+            'password' => 'required',
+        ], [
+            'name.required' => 'El campo nombre es obligatorio'
+        ]);
+        User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password'])
+        ]);
+        return redirect()->route('users.index');
+    }
+    public function edit(User $user)
+    {
+        return view('users.edit', ['user' => $user]);
+    }
+}
